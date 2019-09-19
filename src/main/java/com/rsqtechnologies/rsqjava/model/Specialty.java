7402ef66e650656eaa2a664e006df47c8d9c0be8
Szymon Patora <szymonopatora@gmail.com>
2019-09-19 23:10:37 +0200

    Specialty class created

--- src/main/java/com/rsqtechnologies/rsqjava/model/Specialty.java
+++ src/main/java/com/rsqtechnologies/rsqjava/model/Specialty.java
@@ -1,4 +1,20 @@
 package com.rsqtechnologies.rsqjava.model;
 
-public class Specialty {
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "specialities")
+public class Specialty extends BaseEntity{
+
+    @Column(name = "description")
+    private String description;
 }
