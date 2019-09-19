df367180ff7e121b608e961a6b2cfe3d55b46278
Szymon Patora <szymonopatora@gmail.com>
2019-09-19 22:38:43 +0200

    First commit

---
+++ src/main/java/com/rsqtechnologies/rsqjava/RsqJavaApplication.java
@@ -0,0 +1,13 @@
+package com.rsqtechnologies.rsqjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsqJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsqJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rsqtechnologies/rsqjava/RsqJavaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rsqtechnologies.rsqjava;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RsqJavaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
