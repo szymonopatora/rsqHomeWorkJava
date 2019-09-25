8c3436d078914a05e5353249a2024b00f7f2760d
Szymon Patora <szymonopatora@gmail.com>
2019-09-25 12:46:31 +0200

    MedicServiceImplTest created. @Builder in Medic created

--- src/main/java/com/rsqtechnologies/rsqjava/model/Medic.java
+++ src/main/java/com/rsqtechnologies/rsqjava/model/Medic.java
@@ -15,6 +15,12 @@ import java.util.Set;
 @Table(name = "medics")
 public class Medic extends Person {
 
+    @Builder
+    public Medic(Long id, String firstName, String lastName, Set<Specialty> specialties) {
+        super(id, firstName, lastName);
+        this.specialties = specialties;
+    }
+
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "medic_specialties", joinColumns = @JoinColumn(name = "medic_id"),
             inverseJoinColumns = @JoinColumn(name = "speciality_id"))
---
+++ src/test/java/com/rsqtechnologies/rsqjava/service/serviceImpl/MedicServiceImplTest.java
@@ -0,0 +1,57 @@
+package com.rsqtechnologies.rsqjava.service.serviceImpl;
+
+import com.rsqtechnologies.rsqjava.model.Medic;
+import com.rsqtechnologies.rsqjava.repository.MedicRepository;
+import com.rsqtechnologies.rsqjava.repository.SpecialtyRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class MedicServiceImplTest {
+
+    private final String FIRST_NAME = "John";
+    private final String LAST_NAME = "Smith";
+
+
+    @Mock
+    MedicRepository medicRepository;
+
+    @Mock
+    SpecialtyRepository specialtyRepository;
+
+    @InjectMocks
+    MedicServiceImpl medicService;
+
+    Medic medic;
+
+    @BeforeEach
+    void setUp() {
+        medic = Medic.builder().id(1L)
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
