b8323d61f7e8bb353a7788df6125ec9ac945918d
Szymon Patora <szymonopatora@gmail.com>
2019-10-01 11:54:18 +0200

    showMedics.html created. getAllMedics() in MedicController created

--- src/main/java/com/rsqtechnologies/rsqjava/controller/MedicController.java
+++ src/main/java/com/rsqtechnologies/rsqjava/controller/MedicController.java
@@ -1,14 +1,22 @@
 package com.rsqtechnologies.rsqjava.controller;
 
-import com.rsqtechnologies.rsqjava.repository.MedicRepository;
+import com.rsqtechnologies.rsqjava.service.MedicService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MedicController {
 
-    private final MedicRepository medicRepository;
+    private final MedicService medicService;
 
-    public MedicController(MedicRepository medicRepository) {
-        this.medicRepository = medicRepository;
+    public MedicController(MedicService medicService) {
+        this.medicService = medicService;
+    }
+
+    @RequestMapping("/medic/medicIndex")
+    public String getAllMedics (Model model) {
+        model.addAttribute("medics", medicService.findAll());
+        return "medic/showMedics";
     }
 }
